630fc62605d3eb1b8a6c969bad4058c702a5a77e
GaryLore <garyperez1369@gmail.com>
2025-03-20 19:45:43 -0400

    Update dark_mode.dart(black text for light mode)
    set brightness as dark to be used as value to check if in dark or light mode

--- lib/theme/dark_mode.dart
+++ lib/theme/dark_mode.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 ThemeData darkMode = ThemeData(
+  brightness: Brightness.dark,
   colorScheme: ColorScheme.dark(
     surface: Colors.grey.shade900,
     primary: Colors.grey.shade600,
